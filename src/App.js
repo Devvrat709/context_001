9c766f02f88b9fead41e25c71d9caa6b2343ac2d
Dev vrat <devvrat709@gmail.com>
2023-03-10 22:21:55 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Header from './components/Header'
+import Contact from './components/Contact'
+import {
+  Routes, Route
+} from 'react-router-dom'
+import Service from './components/Service'
+import { ThemeContext } from './theme-context'
+
+
 
 function App() {
+  
+  const { theme, toggle, dark } = React.useContext(ThemeContext)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+
+    <div >
+      <header
+        style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
+      >
+        <Header />
+        <button
+          type="button"
+          onClick={toggle}
+          style={{
+            backgroundColor: theme.backgroundColor,
+            color: theme.color,
+            outline: 'none'
+          }}
         >
-          Learn React
-        </a>
+          Toggle to {!dark ? 'Dark' : 'Light'} theme
+        </button>
+        <br/>
+        <Routes>
+          <Route exact path='/' element={
+            <>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </>
+          } />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/service' element={<Service />} />
+        </Routes>
+
       </header>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
